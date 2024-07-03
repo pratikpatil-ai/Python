f1718c194fa48bec0908b14ef130d639ae8998d5
pratikpatil-ai <pratikpatil.ai@gmail.com>
2024-07-03 15:39:32 +0530

    chapter4

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,9 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Pratik", "Kartik"]
+
+
+print(friends[0])
+
+friends[0] = "Grapes" # Unlike Strings Lists are , mutable
+
+print(friends[6])
+print(friends[1:4])
---
+++ Chapter 4/03_tuple.py
@@ -0,0 +1,4 @@
+a = (1,45,342,3424,False,"Rohan", "Shivam")
+
+print(a)
+print(type(a))
